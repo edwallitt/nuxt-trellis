d439f28a040945b843a6d85a0a619d176b3c2941
Dr Ed Wallitt <ed@wallitt.io>
2020-08-16 23:25:33 +0100

    Change URL to support my json server

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000`,
+  baseURL: `http://my-json-server.typicode.com/edwallitt/nuxt-trellis`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
